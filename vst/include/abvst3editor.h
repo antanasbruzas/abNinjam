922d53edaff74adc69ef4180843e1eda25d1c4de
Antanas Bruzas <antanas@hippie.lt>
2020-06-05 10:49:33 +0300

    Bitwig 3.2 fixes black window issue so workaround is not needed anymore

--- vst/include/abvst3editor.h
+++ vst/include/abvst3editor.h
@@ -9,11 +9,6 @@
 #include "plugids.h"
 
 #include "vstgui/plugin-bindings/vst3editor.h"
-#if !defined(__APPLE__) && !defined(_WIN32)
-namespace VSTGUI {
-class RunLoop;
-}
-#endif
 
 using namespace VSTGUI;
 
@@ -24,7 +19,6 @@ class AbVST3Editor : public VST3Editor {
 public:
   AbVST3Editor(Steinberg::Vst::EditController *controller,
                UTF8StringPtr templateName, UTF8StringPtr xmlFile);
-  CMessageResult notify(CBaseObject *sender, IdStringPtr message) override;
   void valueChanged(CControl *pControl) override;
   void controlEndEdit(VSTGUI::CControl *pControl) override;
 
--- vst/src/abvst3editor.cpp
+++ vst/src/abvst3editor.cpp
@@ -1,9 +1,5 @@
 #include "../include/abvst3editor.h"
 
-#if !defined(__APPLE__) && !defined(_WIN32)
-#include "../include/X11RunLoop.h"
-#endif
-
 using namespace VSTGUI;
 using namespace AbNinjam;
 using namespace Vst3;
@@ -14,34 +10,6 @@ AbVST3Editor::AbVST3Editor(Steinberg::Vst::EditController *editController,
                            UTF8StringPtr xmlFileName)
     : VST3Editor(editController, templateName, xmlFileName) {
   L_(ltrace) << "[AbVST3Editor] Entering AbVST3Editor::AbVST3Editor";
-}
-
-CMessageResult AbVST3Editor::notify(CBaseObject *sender, IdStringPtr message) {
-  // L_(ltrace) << "[AbVST3Editor] Entering AbVST3Editor::notify";
-  if (message == CVSTGUITimer::kMsgTimer) {
-    if (doCreateView)
-      recreateView();
-  }
-  CMessageResult result = VSTGUIEditor::notify(sender, message);
-
-  if (result != kMessageNotified)
-    return result;
-
-#if !defined(__APPLE__) && !defined(_WIN32)
-  if (message == CVSTGUITimer::kMsgTimer) {
-    SharedPointer<RunLoop> runLoop = RunLoop::get();
-    if (runLoop) {
-      // note(jpc) I don't find a reliable way to check if the host
-      //   notifier of X11 events is working. If there is, remove this and
-      //   avoid polluting Linux hosts which implement the loop correctly.
-      runLoop->processSomeEvents();
-
-      runLoop->cleanupDeadHandlers();
-    }
-  }
-#endif
-
-  return result;
 }
 
 void AbVST3Editor::valueChanged(CControl *pControl) {
